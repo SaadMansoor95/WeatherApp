b9a09ffe44ad607d6fe56510169e7d0eba0b4497
SaadMansoor95 <saadi@live.com>
2022-03-28 23:44:30 +0500

    Creating Models

---
+++ WeatherApp/Model/City.cs
@@ -0,0 +1,19 @@
+﻿namespace WeatherApp.Model
+{
+    internal class City
+    {
+        public int Version { get; set; }
+        public string Key { get; set; }
+        public string Type { get; set; }
+        public int Rank { get; set; }
+        public string LocalizedName { get; set; }
+        public Area Country { get; set; }
+        public Area AdministrativeArea { get; set; }
+    }
+
+    public class Area
+    {
+        public string ID { get; set; }
+        public string LocalizedName { get; set; }
+    }
+}
