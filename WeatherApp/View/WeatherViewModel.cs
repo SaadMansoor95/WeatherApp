b01b6dac4ff7ec47cb5c501e175997b0ab9cba22
SaadMansoor95 <saadi95@outlook.com>
2022-03-29 14:21:58 +0500

    Weather Ui Created, ViewModel using INotiftPropertyChange

---
+++ WeatherApp/View/WeatherViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace WeatherApp.View
+{
+    public class WeatherViewModel : INotifyPropertyChanged
+    {
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set 
+            { 
+                query = value; 
+                OnPropertyChange(query); 
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChange(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
